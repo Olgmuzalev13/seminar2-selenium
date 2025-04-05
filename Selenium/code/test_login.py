5f63723389b391578c6e3b5f889c1b4ac4f0f183
OlgMuzalev <razrushitelyvselennoy@mail.ru>
2025-04-05 21:41:16 +0300

    constants

--- Selenium/code/test_login.py
+++ Selenium/code/test_login.py
@@ -4,6 +4,9 @@ from _pytest.fixtures import FixtureRequest
 import time
 from ui.vk_pages.base_page import BasePage
 
+QA_TOPIC_ID = "2459"
+QA2_SEMINAR_ID = "30921"
+ILIA_ADDRESS = "i.andriianov"
 
 class BaseCase:
     authorize = False
@@ -34,14 +37,14 @@ class TestLK(BaseCase):
 
     def test_get_seminar_info(self):
         assert 'Прямой эфир' in self.driver.page_source 
-        self.base_page.get_topic_page_by_id(id="2459")
+        self.base_page.get_topic_page_by_id(id=QA_TOPIC_ID)
         self.base_page.click(vk_locators.MainPageLocators.LESSONS, timeout=10)
         assert 'Лекция 1' in self.driver.page_source
-        self.base_page.get_lesson_by_id(id="30921")
+        self.base_page.get_lesson_by_id(id=QA2_SEMINAR_ID)
         assert 'End-to-End тесты на Python' in self.driver.page_source
 
     def test_search_Ilia(self): 
         self.base_page.click(vk_locators.MainPageLocators.SEARCH_OPEN, timeout=10)
         self.base_page.search("Андриянов Илья\n")
-        self.base_page.get_student_by_address(address="i.andriianov")
+        self.base_page.get_student_by_address(address=ILIA_ADDRESS)
         assert self.driver.title =="Профиль - Илья Андриянов - VK Education"
